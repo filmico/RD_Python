7b74103e090659ee74cbbbb1b67a17ef0ef34ff4
Gerardo Verrone <animatorstudio@hotmail.com>
2018-07-08 11:41:10 +0100

    W4 Actualizacion startup.py y userSetup.py
    Se actualizaron los archivos para que Maya abra los puertos para los IDE externos

--- startup.py
+++ startup.py
@@ -2,6 +2,28 @@ import maya.cmds as cmds
 
 print 'Startup! - C:\RDojo_Python101\Repo\startup.py'
 
+# Open Port to External IDE
+if not cmds.commandPort(':4434', query=True):
+    cmds.commandPort(name=':4434', sourceType="python")
+    print 'Port :4434 is open for IDE -> Python!'
+
+if not cmds.commandPort(':7001', query=True):
+    cmds.commandPort(name=':7001', sourceType="mel")
+    print 'Port :7001 is open for IDE -> Mel!'
+
+if not cmds.commandPort(':7002', query=True):
+    cmds.commandPort(name=':7002', sourceType="python")
+    print 'Port :7002 is open for IDE -> Python!'
+
+# Change the current time unit to ntsc
+cmds.currentUnit(time='ntsc')
+
+# Change the current linear unit to inches
+cmds.currentUnit(linear='cm')    
+
+# Agregamos los path que necesitamos
+# sys.path.append('C:/Rigging Dojo - Python 101 (On Demand)/RD_Python/')
+
 # Let's import the UI  (folderName.PythonFile)
 import ui.ui as ui
-reload(ui)  # left it like this in develop mode. It always load the .py instead of the .pyc
+# reload(ui)  # left it like this in develop mode. It always load the .py instead of the .pyc
--- userSetup.py
+++ userSetup.py
@@ -4,20 +4,6 @@ import maya.cmds as cmds
 
 print "In Rig Tool - C:\RDojo_Python101\Repo\userSetup.py"
 
-# Open Port to External IDE
-if not cmds.commandPort(':4434', query=True):
-    cmds.commandPort(n=':4434')
-    print 'Port :4434 is open!'
-
-# Change the current time unit to ntsc
-cmds.currentUnit(time='ntsc')
-
-# Change the current linear unit to inches
-cmds.currentUnit(linear='cm')    
-
-# Agregamos los path que necesitamos
-# sys.path.append('C:/Rigging Dojo - Python 101 (On Demand)/RD_Python/')
-
 # Importamos el script de startup del folder de trabajo, pero solo cuando el maya haya terminado de cargar todo
 # import deferred trabaja cuando el maya se pone en modo idle
 cmds.evalDeferred('import startup')
