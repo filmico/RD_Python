f1ccdf51bd5d2f9bb28c038254ea01ca52f41e48
Gerardo Verrone <animatorstudio@hotmail.com>
2018-07-19 11:48:33 +0100

    W5 Avance Script previo a guardar JSon

--- system/utils.py
+++ system/utils.py
@@ -1,103 +1,74 @@
 import maya.cmds as cmds
-from pymel.core import *
-import json
-import tempfile
+import json as json
+
+# Variable de Entorno seteada en el modulo Riggint_tool.mod
+
+# import os 
+# print os.environ['RIGGING_TOOL']	
+
+
+# JSON
+# =====
 
 def writeJson(fileName, data):
-    with open(fileName, 'w') as outfile:
-        json.dump(data, outfile)
-    file.close(outfile)
+	with open(fileName, 'w') as outFile:
+		json.dump(data, outFile)
+	file.close(outFile)
 
 def readJson(fileName):
-    with open(fileName, 'r') as infile:
-        data = (open(infile.name, 'r').read())
-    return data
+	with open(fileName, 'r') as inFile:
+		data = (open(inFile.name, 'r').read())
+	return data
 
+'''
+	JSON How to Use it
+	------------------
 
-def createJoint(name, position, instance):
-    # Use a list comprehension to build joints.
-    jnt_list = [cmds.joint(n=name[i].replace('_s_', instance), p=position[i]) for i in range(len(name))]
-    cmds.select(d=True)
-    return(jnt_list)
+	import maya.cmds as cmds
+	import os
+	import system.utils as utils
+	import json
+	reload (system.utils)
 
+	path = os.environ['RIGGING_TOOL']
+	pathJson = path + '/layout/'
 
-def createControl(ctrlinfo):
-    control_info = []
-    for info in ctrlinfo:
-        # Create ik control
-        # Get ws position of wrist joint
-        pos = info[0]
-        # Create an empty group
-        ctrlgrp = cmds.group( em=True, name='grp_' + info[1] )
-        # Create circle control object
-        ctrl = cmds.circle( n=info[1], nr=(0, 0, 1), c=(0, 0, 0) )
-        # Parent the control to the group
-        cmds.parent(ctrl, ctrlgrp)
-        # Move the group to the joint
-        cmds.xform(ctrlgrp, t=pos, ws=True)
-        control_info.append([ctrlgrp, ctrl[0]])
-    return(control_info)
+	# Lista
+	# ------
+	ik_Jnt_Lst = [['ik_shoulder_jnt', [0, 0, 0]], 
+	            ['ik_elbow_jnt', [3, 0, -1]], 
+	            ['ik_wrist_jnt', [6, 0, 0]], 
+	            ['ik_wristEnd_jnt', [8, 0, 0]] ]
 
+	# Graba
+	utils.writeJson(pathJson + 'lista.json', armIKCtrl) 
+	       
+	# Lee
+	ik_Jnt_Lst_De_Json = utils.readJson(pathJson + 'lista.json')
+	print 'Lista: ' + str(ik_Jnt_Lst_De_Json)
 
-def calculatePVPosition(jnts):
-    from maya import cmds , OpenMaya
-    start = cmds.xform(jnts[0] ,q=True ,ws=True, t=True)
-    mid = cmds.xform(jnts[1] ,q=True ,ws=True, t=True)
-    end = cmds.xform(jnts[2] ,q=True ,ws=True, t=True)
-    startV = OpenMaya.MVector(start[0] ,start[1],start[2])
-    midV = OpenMaya.MVector(mid[0] ,mid[1],mid[2])
-    endV = OpenMaya.MVector(end[0] ,end[1],end[2])
-    startEnd = endV - startV
-    startMid = midV - startV
-    dotP = startMid * startEnd
-    proj = float(dotP) / float(startEnd.length())
-    startEndN = startEnd.normal()
-    projV = startEndN * proj
-    arrowV = startMid - projV
-    arrowV*= 0.5
-    finalV = arrowV + midV
-    return ([finalV.x , finalV.y ,finalV.z])
+	            
+	# Diccionario
+	# -----------
+	armIKCtrl = {}          
+	                       # offsetGrp_name, Ctrl_name, Ctrl_normal, Ctrl_radius, Ctrl_position, Ctrl_rotation
+	armIKCtrl['ikh_arm'] = [ik_Jnt_Lst[0][0], ik_Jnt_Lst[2][0], 'ikh_arm', 'ikRPsolver', 2, 1] 
 
-def connectThroughBC(parentsA, parentsB, children, instance, switchattr ):
-    constraints = []
-    for j in range(len(children)):
-        switchPrefix = children[j].partition('_')[2]
-        bcNodeT = cmds.shadingNode("blendColors", asUtility=True, n='bcNodeT_switch_' + switchPrefix)
-        cmds.connectAttr(switchattr, bcNodeT  + '.blender')
-        bcNodeR = cmds.shadingNode("blendColors", asUtility=True, n='bcNodeR_switch_' + switchPrefix)
-        cmds.connectAttr(switchattr, bcNodeR  + '.blender')
-        bcNodeS = cmds.shadingNode("blendColors", asUtility=True, n='bcNodeS_switch_' + switchPrefix)
-        cmds.connectAttr(switchattr, bcNodeS  + '.blender')
-        constraints.append([bcNodeT, bcNodeR, bcNodeS])
-        # Input Parents
-        cmds.connectAttr(parentsA[j] + '.translate', bcNodeT + '.color1')
-        cmds.connectAttr(parentsA[j] + '.rotate', bcNodeR + '.color1')
-        cmds.connectAttr(parentsA[j] + '.scale', bcNodeS + '.color1')
-        if parentsB != 'None':
-            cmds.connectAttr(parentsB[j] + '.translate', bcNodeT + '.color2')
-            cmds.connectAttr(parentsB[j] + '.rotate', bcNodeR + '.color2')
-            cmds.connectAttr(parentsB[j] + '.scale', bcNodeS + '.color2')
-        # Output to Children
-        cmds.connectAttr(bcNodeT + '.output', children[j] + '.translate')
-        cmds.connectAttr(bcNodeR + '.output', children[j] + '.rotate')
-        cmds.connectAttr(bcNodeS + '.output', children[j] + '.scale')
-    return constraints
+	# Graba
+	utils.writeJson(pathJson + 'diccionario.json', armIKCtrl)
 
+	# Lee
+	armIKCtrl_De_Json = utils.readJson(pathJson + 'diccionario.json')
 
-def findHighestTrailingNumber(names, basename):
-    import re
-    highestValue = 0
+	# Convierte a diccionario (json.loads)
+	armIKCtrl_Nuevo = {}
+	armIKCtrl_Nuevo = json.loads(armIKCtrl_De_Json)
 
-    for n in names:
-        if objExists(n) == True:
-            n = str(n)
-            if n.find(basename) >= 0:
-                suffix = n.partition(basename)[2][0]
+	print 'Complete Diccionary: ' + str(armIKCtrl_Nuevo)
 
-                if re.match("^[0-9]*$", suffix):
-                    numericalElement = int(suffix)
+	for key, value in armIKCtrl_Nuevo.iteritems():
+		print 'Key: ' + str(key), 'Values: ' + str(value)	
+		
+	print armIKCtrl['ikh_arm'][2]		
 
-                    if numericalElement >= highestValue:
-                        highestValue = numericalElement + 1
-        else:
-            highestValue = 1
+'''
